ebd62f861f28cb06746407f2affb5f54105891b2
Azis Adi Kuncoro <azisadikuncoro@gmail.com>
2021-06-29 21:49:26 +0700

    fix build error

--- components/Toggle.js
+++ components/Toggle.js
@@ -16,13 +16,10 @@ export default function Toggle(props) {
     }
   }
 
-  // <div className="fixed top-3.5 right-3.5">
-  //   <button className="bg-gray-300" onClick={toggle}>Toggle</button>
-  // </div>
   return (
-      <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-        <input defaultChecked={checked} onClick={toggle}  type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
-        <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+      <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+        <input defaultChecked={checked} onClick={toggle}  type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+        <label htmlFor="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
       </div>
   )
 }
